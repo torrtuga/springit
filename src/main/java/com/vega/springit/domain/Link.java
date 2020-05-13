39f670ecc558b223e5b6b969f28b12c29e06c21f
Arqam Ahmad <arqam.ahmad@freshtohome.com>
2020-05-14 05:07:45 +0530

    creating entities

---
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -0,0 +1,26 @@
+package com.vega.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Data
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String url;
+}
